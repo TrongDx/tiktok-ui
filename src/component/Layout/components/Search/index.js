d39d7d8363c0efa5311a8b4c3b61c660d59f69fb
TrongDx <xuantrong02032002@gmail.com>
2023-09-08 20:51:41 +0700

    build Header #3, useDebounce để bớt request search

--- src/component/Layout/components/Search/index.js
+++ src/component/Layout/components/Search/index.js
@@ -6,6 +6,7 @@ import AccountItem from '~/component/AccountItem';
 import { faCircleXmark, faSpinner, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'; // Import the correct icon 
 import HeadlessTippy from '@tippyjs/react/headless';
 import 'tippy.js/dist/tippy.css';
+import { useDebounce } from '~/hooks';
 import styles from './Search.module.scss';
 
 const cx = classNames.bind(styles)
@@ -15,18 +16,20 @@ function Search() {
     const [showResult, setShowResult] = useState(true)
     const [loading, setLoading] = useState(false)
 
+    const debounced = useDebounce(searchValue, 500)
+
     const inputRef = useRef()
 
 
     useEffect(() => {
-        if (!searchValue.trim()) {
+        if (!debounced.trim()) {
             setSearchResult([])
             return;
         }
 
         setLoading(true)
 
-        fetch(`https://tiktok.fullstack.edu.vn/api/users/search?q=${encodeURIComponent(searchValue)}&type=less`)
+        fetch(`https://tiktok.fullstack.edu.vn/api/users/search?q=${encodeURIComponent(debounced)}&type=less`)
             .then(res => res.json())
             .then(res => {
                 setSearchResult(res.data);
@@ -35,7 +38,7 @@ function Search() {
             .catch(() => {
                 setLoading(false)
             })
-    }, [searchValue])
+    }, [debounced])
 
     const handleHideResult = () => {
         setShowResult(false)
