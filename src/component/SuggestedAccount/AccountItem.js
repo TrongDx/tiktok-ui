68608201960951ce7928b2423397ca934cd78187
TrongDx <xuantrong02032002@gmail.com>
2023-09-24 21:32:55 +0700

    update AccountPreview

--- src/component/SuggestedAccount/AccountItem.js
+++ src/component/SuggestedAccount/AccountItem.js
@@ -3,50 +3,78 @@ import classNames from "classnames/bind";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
 import Tippy from '@tippyjs/react/headless';
+import { useEffect, useState } from "react";
 
 import { Wrapper as PopperWrapper } from '~/component/Popper'
 import styles from './SuggestedAccount.module.scss'
 import AccountPreview from './AccountPreview';
 import Image from '../Image/Image';
+import { db } from '~/component/firebase/firebase'
 
 const cx = classNames.bind(styles)
 function AcountItem() {
-    const renderPreview = (props) => {
-        return (
-            <div tabIndex='-1' {...props}>
-                <PopperWrapper>
-                    <AccountPreview />
-                </PopperWrapper>
-            </div>
-        )
-    }
+    // const renderPreview = (props) => {
+    //     return (
+    //         <div tabIndex='-1' {...props}>
+    //             <PopperWrapper>
+    //                 <AccountPreview />
+    //             </PopperWrapper>
+    //         </div>
+    //     )
+    // }
 
+    const [videos, setVideos] = useState([])
+    useEffect(() => {
+        db.collection("videos").get().then((querySnapshot) => {
+            // setVideos(querySnapshot.docs.map(doc => doc.data()))
+            const videoData = querySnapshot.docs.map((doc) => {
+                return {
+                    id: doc.id, // Sử dụng ID tự động của Firestore làm giá trị "key"
+                    ...doc.data(), // Lấy dữ liệu của tài liệu
+                };
+            });
+            setVideos(videoData);
+        })
+    }, []);
     return (
-        <span>
-            <Tippy
-                // visible
-                interactive
-                delay={[800, 0]}
-                offset={[-20, -10]}
-                placement='bottom'
-                render={renderPreview}
-            >
-                <div className={cx('account-item')}>
-                    <Image
-                        className={cx('avatar')}
-                        src="https://p16-sign-va.tiktokcdn.com/tos-maliva-avt-0068/a13a343f85e50b6af1e1f80c95f0accf~c5_100x100.jpeg?x-expires=1695214800&x-signature=Ujejcxy88Ukn%2Bpy1JkRdcRddYFU%3D"
-                        alt="/"
-                    />
-                    <div className={cx('item-info')}>
-                        <p className={cx('nickname')}>
-                            <strong>dthue_312</strong>
-                            <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />
-                        </p>
-                        <p className={cx('name')}>by Đ.Huệ</p>
+        // {
+        videos.map((video) => (
+            <span key={video.id}>
+                <Tippy
+                    // visible
+                    interactive
+                    delay={[800, 0]}
+                    offset={[-20, -10]}
+                    placement='bottom'
+                    render={(props) => (
+                        <div tabIndex='-1' {...props}>
+                            <PopperWrapper >
+                                <AccountPreview data={video} />
+                            </PopperWrapper>
+                        </div>
+                    )}
+                >
+
+                    <div className={cx('account-item')}>
+                        <Image
+                            className={cx('avatar')}
+                            src={video.avatar}
+                            alt="/"
+                        />
+                        <div className={cx('item-info')}>
+                            <p className={cx('nickname')}>
+                                <strong>{video.nickname}</strong>
+                                <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />
+                            </p>
+                            <p className={cx('name')}>{video.fullname}</p>
+                        </div>
                     </div>
-                </div>
-            </Tippy>
-        </span>
+
+                </Tippy>
+
+            </span>
+        ))
+        // }
     );
 }
 
--- src/component/SuggestedAccount/AccountPreview/AccountPreview.js
+++ src/component/SuggestedAccount/AccountPreview/AccountPreview.js
@@ -8,23 +8,23 @@ import Image from '~/component/Image/Image';
 
 const cx = classNames.bind(styles)
 
-function AccountPreview() {
+function AccountPreview({ data }) {
     return (
         <div className={cx('wrapper')}>
             <div className={cx('header')}>
-                <Image className={cx('avatar')} src="https://p16-sign-va.tiktokcdn.com/tos-maliva-avt-0068/a13a343f85e50b6af1e1f80c95f0accf~c5_100x100.jpeg?x-expires=1695214800&x-signature=Ujejcxy88Ukn%2Bpy1JkRdcRddYFU%3D" alt="" />
+                <Image className={cx('avatar')} src={data.avatar} alt="" />
                 <Button className={cx('follow-btn')} primary>Follow</Button>
             </div>
             <div className={cx('body')}>
                 <p className={cx('nickname')}>
-                    <strong>dthue_312</strong>
+                    <strong>{data.nickname}</strong>
                     <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />
                 </p>
-                <p className={cx('name')}>by Đ.Huệ</p>
+                <p className={cx('name')}>{data.fullname}</p>
                 <p className={cx('analytics')}>
-                    <strong className={cx('value')}>8.2M </strong>
+                    <strong className={cx('value')}>{data.follow} </strong>
                     <span className={cx('label')}>Follow</span>
-                    <strong className={cx('value')}>8.2M </strong>
+                    <strong className={cx('value')}>{data.like}</strong>
                     <span className={cx('label')}>Like</span>
                 </p>
             </div>
